d0f7e16149550db3af34abb46cd5dd7c1026f932
MichaelHabermas <michaelghabermas@gmail.com>
2021-05-14 09:44:23 -0400

    basic reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,24 @@
-
 export const initialState = {
-}
+	//Task List: X
+	//1. Adds the following state values into the initialState: X
+	//  - an array of smurfs X
+	smurfs: [],
+	//  - a boolean indicating if the app is loading X
+	isLoading: false,
+	//  - a string indicating a possible error message X
+	errorMessage: ''
+};
 
-const reducer = ()=>{
-}
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		default:
+			return state;
+	}
+};
 
 export default reducer;
 
-//Task List:
-//1. Adds the following state values into the initialState:
-//  - an array of smurfs
-//  - a boolean indicating if the app is loading
-//  - a string indicating a possible error message
-
-//2. Add in the arguments needed to complete a standard reducer function.
+//2. Add in the arguments needed to complete a standard reducer function. X
 //3. Add in a reducer case to accomidate the start of a smurf fetch.
 //4. Add in a reducer case to accomidate the successful smurf api fetch.
 //5. Add in a reducer cases to accomidate the failed smurf api fetch.
